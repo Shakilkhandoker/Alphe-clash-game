4e306378388944e9a28fbb6bf7942c15de5f01b8
Shakilkhandoker <shakilkhandoker1@gmail.com>
2024-03-28 01:11:55 +0600

    push the project in git

--- script/alpha.js
+++ script/alpha.js
@@ -12,9 +12,7 @@
 //   playScreen.classList.remove('hidden');
 // })
 
-document
-  .getElementById("home-screen-btn")
-  .addEventListener("click", function () {
+document.getElementById("home-screen-btn").addEventListener("click", function () {
     hideScreen("home-screen");
     showScreen("play-ground");
     continueGame();
@@ -37,10 +35,38 @@ document.addEventListener("keyup", function (event) {
     // !1.get the current score
     // !2.increase the c=score by 1
     // !3.show the updated score
-    
+    const currentScore = document.getElementById("current-score");
+    currentScore.innerText = parseInt(currentScore.innerText) + 1;
 
     // !continue the game again
     removeRandomAlphabetButton(targetKey);
     continueGame();
+  } 
+  else {
+    // !1.get the current life number
+    // !2.reduce the life count when press the wrong key
+    // !3.display the updated life count
+    const currentLife = document.getElementById("current-life");
+    currentLife.innerText = parseInt(currentLife.innerText) - 1;
+
+    // !when life is 0,hide the play ground and show the score board
+    if (parseInt(currentLife.innerText) === 0) {
+      hideScreen("play-ground");
+      showScreen("score-board");
+    }
   }
 });
+
+document.getElementById('play-again').addEventListener('click',function(){
+  // !hide everything and show the playground
+  hideScreen("score-board");
+  showScreen("play-ground");
+  continueGame();
+
+
+  // !update final score
+  // !get the final score
+  // !set the score in the final score board
+
+})
+
--- script/utility.js
+++ script/utility.js
@@ -1,32 +1,55 @@
 // todo- hide the home screen
-function hideScreen(elementId){
-  const element=document.getElementById(elementId)
-  element.classList.add('hidden')
+function hideScreen(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.add("hidden");
 }
 
 // todo-show the screen
-function showScreen(elementId){
-  const element=document.getElementById(elementId)
-  element.classList.remove('hidden')
+function showScreen(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.remove("hidden");
 }
 
 // todo-get random alphabet
 
-function getRandomAlphabet(){
-  const alphabet = 'abcdefghijklmnopqrstuvwxyz'
-  return alphabet[Math.floor(Math.random() * alphabet.length)]
+function getRandomAlphabet() {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  return alphabet[Math.floor(Math.random() * alphabet.length)];
 }
 
 // todo-set the random alphabet button color
-function setRandomAlphabetButton(elementId){
-  const element=document.getElementById(elementId)
-  element.classList.add('bg-orange-500')
+function setRandomAlphabetButton(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.add("bg-orange-500");
 }
 
 // todo-remove the random alphabet button color
 
-function removeRandomAlphabetButton(elementId){
-  const element=document.getElementById(elementId)
-  element.classList.remove('bg-orange-500')
+function removeRandomAlphabetButton(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.remove("bg-orange-500");
 }
 
+// todo-current life
+function setCurrentLife(elementId, value) {
+  const element = document.getElementById(elementId);
+  element.innerText = value;
+}
+
+// todo-current score
+function setCurrentScore(elementId, value) {
+  const element = document.getElementById(elementId);
+  element.innerText = value;
+}
+
+function getTextElementValueById(elementId) {
+  const element = document.getElementById(elementId);
+  const elementValueText = element.innerText;
+  const value = parseInt(elementValueText);
+  return value;
+}
+
+function setTextElementValueById(elementId, value) {
+  const element = document.getElementById(elementId);
+  element.innerText = value;
+}
